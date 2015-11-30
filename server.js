43de18e51347c16b92bf060d509cf679e7acf997
1303721 <mrdrift@hotmail.co.uk>
2015-11-30 14:05:52 +0000

    Rejected commit: Default

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 
 http.createServer(function(request, response){
-    response.writeHead(200, { 'Content-type': 'text/plain'}):
-    response.end('Hello World\n');
+    response.writeHead(200, { 'Content-type': 'text/plain'});
+    response.end('Hello World test \n');
 }).listen(post);
