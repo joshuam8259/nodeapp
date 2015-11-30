bd15d17885446deb5efa36412d1443d6b320c0d1
1303721 <mrdrift@hotmail.co.uk>
2015-11-30 14:09:49 +0000

    Rejected commit: Default

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /**
  * Created by 1303721 on 30/11/2015.
  */
-var http = require('http')
+var http = require('http');
 var port = process.env.PORT || 1337;
 
 http.createServer(function(request, response){
