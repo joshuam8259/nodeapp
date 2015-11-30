54936e9f76f589cf730e9ff92fee0b99e1763dd5
1303721 <mrdrift@hotmail.co.uk>
2015-11-30 14:36:00 +0000

    Rejected commit: Default

---
+++ connect.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 1303721 on 30/11/2015.
+ */
+
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
