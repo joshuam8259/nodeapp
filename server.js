39d28a80b009be6af39f6434ebec90020ce7c43e
1303721 <mrdrift@hotmail.co.uk>
2015-11-30 13:55:35 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 1303721 on 30/11/2015.
+ */
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response){
+    response.writeHead(200, { 'Content-type': 'text/plain'}):
+    response.end('Hello World\n');
+}).listen(post);
