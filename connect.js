f1e248173ffcd996fe9905046aaa5676c93e7207
1303721 <mrdrift@hotmail.co.uk>
2015-11-30 14:40:41 +0000

    Rejected commit: Default

--- connect.js
+++ connect.js
@@ -8,3 +8,7 @@ var mongodb = require('mongodb');
 var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
+// Connection URL.This is where your mongodb server is running.
+var url = 'mongodb://<1303721>:<greencar23>@ds054118.mongolab.com:54118/nodeapp';
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
